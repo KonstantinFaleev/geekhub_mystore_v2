860d6fcfc14ff748ffdbe6fbb74c951948fa6e3f
KonstantinFaleev <konstantin.faleev@gmail.com>
2017-12-31 18:30:09 +0200

    Check Out!

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,17 @@
 class Order < ApplicationRecord
+  has_many :line_items, dependent: :destroy
   enum pay_type: {
       "New" => 0,
       "In processing" => 1,
       "Sent" => 2
   }
+
+  validates :name, :phone, :email, presence: true
+
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+      end
+    end
 end
