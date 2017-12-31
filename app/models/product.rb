643a0b095757edb2a491e947c86d6ea2d5dcd9b4
KonstantinFaleev <konstantin.faleev@gmail.com>
2017-12-31 18:50:28 +0200

    Atom Feeds

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,21 @@
 class Product < ApplicationRecord
   has_many :line_items
+  has_many :orders, through: :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
   mount_uploaders :images, ProductImageUploader
   validates :title, :description, :images, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
+
+  def who_bought
+    @product = Product.find(params[:id])
+    @latest_order = @product.orders.order(:updated_at).last
+    if stale?(@latest_order)
+      respond_to do |format|
+        format.atom
+      end
+    end
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
