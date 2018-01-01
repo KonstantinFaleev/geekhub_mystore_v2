446fe4b6e21d82925dd99289bd3cf2700dbd3ce4
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-01-01 15:43:13 +0200

    Pagination Kaminari

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,8 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    #@products = Product.all
+    @products = Product.order("title").page(params[:page]).per(3)
   end
 
   # GET /products/1
