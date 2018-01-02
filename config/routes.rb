a65fbe32d57d8f3809846a783be0322575f97d42
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-01-02 14:13:10 +0200

    +- show product , href

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :orders
+  resources :orders, only: [:new, :create]
   resources :line_items
   resources :carts
   root 'store#index', as: 'store_index'
