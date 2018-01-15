cb381173b18ec717ab32791a5babfbc0cac0237f
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-01-15 22:59:48 +0200

    model category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171231144404) do
+ActiveRecord::Schema.define(version: 20180111130647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,12 @@ ActiveRecord::Schema.define(version: 20171231144404) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "line_items", force: :cascade do |t|
     t.bigint "product_id"
     t.bigint "cart_id"
@@ -80,6 +86,8 @@ ActiveRecord::Schema.define(version: 20171231144404) do
     t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category
+_id"
   end
 
   add_foreign_key "line_items", "carts"
